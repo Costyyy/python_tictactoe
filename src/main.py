cc7648e42ee4ccde5aeba2098320dea9fbea4bed
Constantin Suruniuc <costy.suruniuc@gmail.com>
2020-12-08 18:32:10 +0200

    added replay functionality

--- src/main.py
+++ src/main.py
@@ -11,6 +11,7 @@ class TicTacToe:
         self.table_size = table_size
         self.table = np.zeros(shape=(table_size, table_size), dtype='int')
         self.menu_buttons = []
+        self.game_buttons = []
         # self.table[0] = 1
         # self.table[-1] = 2
         pg.init()
@@ -23,10 +24,14 @@ class TicTacToe:
         self.tile_size = 100
         self.width, self.height = self.table_size * self.tile_size, self.table_size * self.tile_size
         self.background = pg.Surface((self.s_width, self.s_height))
-        self.offset = ((800 - self.width) / 2, (600 - self.height) / 2)
+        self.button_surface_size = (self.s_width / 4, self.s_height / 4)
+        self.button_surface = pg.Surface(self.button_surface_size)
+        self.offset = ((self.s_width - self.width) / 2, (self.s_height - self.height) / 2)
+        self.b_offset = ((self.s_width - self.s_width / 4) / 2, (self.s_height - self.s_height / 8) / 1.1)
         self.current_player = 2
         self.human_player = 2
         self.ai_player = 1
+        self.gameover = False
         self.board_tiles = []
         self.player_colors = {1: 'red', 2: 'green'}
         self.init_board()
@@ -45,11 +50,20 @@ class TicTacToe:
                 self.board_tiles[i][j][1] = self.table[i, j]
 
     def draw_board(self):
+        self.background.fill((0, 0, 0))
+        self.button_surface.fill((0, 0, 0))
         for line in self.board_tiles:
             for pos, _ in line:
                 x, y = pos
                 rect = (x, y, self.tile_size, self.tile_size)
                 pg.draw.rect(self.background, 'WHITE', rect, width=1)
+
+        for button in self.game_buttons:
+            if button.name == 'replay_button' and self.gameover:
+                pg.draw.rect(self.button_surface, rect=((button.pos_x, button.pos_y), (button.width, button.height)),
+                             color='WHITE')
+                text = self.smallfont.render(button.text, True, (252, 3, 57))
+                self.button_surface.blit(text, (button.pos_x + button.width / 8, button.pos_y))
 
     def draw_pieces(self):
         for i in range(self.table_size):
@@ -96,12 +110,20 @@ class TicTacToe:
                 self.table[i, j] = self.human_player
                 self.current_player = self.ai_player
 
-    def init_menu(self):
+    def init_menu_buttons(self):
         button_width = 150
         button_height = 50
         button_pos_x = self.s_width / 2 - button_width / 2
         button_pos_y = self.s_height / 2.5 - button_height / 2
         self.menu_buttons.append(Button(button_pos_x, button_pos_y, button_width, button_height, 'play_button', 'PLAY'))
+
+    def init_game_buttons(self):
+        button_width = 150
+        button_height = 50
+        button_pos_x = self.button_surface_size[0] / 2 - button_width / 2
+        button_pos_y = self.button_surface_size[1] / 2 - button_height / 2
+        self.game_buttons.append(
+            Button(button_pos_x, button_pos_y, button_width, button_height, 'replay_button', 'REPLAY'))
 
     def draw_menu(self):
         for button in self.menu_buttons:
@@ -111,7 +133,7 @@ class TicTacToe:
             self.background.blit(text, (button.pos_x + button.width / 4, button.pos_y))
 
     def menu(self):
-        self.init_menu()
+        self.init_menu_buttons()
         while True:
             self.draw_menu()
             for event in pg.event.get():
@@ -134,6 +156,7 @@ class TicTacToe:
     def run(self):
         game_exit = False
         self.background = pg.Surface((self.width, self.height))
+        self.init_game_buttons()
         while not game_exit:
             self.update_board()
             self.draw_board()
@@ -141,14 +164,14 @@ class TicTacToe:
             result = check_end(self.table)
             if result == self.ai_player:
                 print('ai win')
-                break
+                self.gameover = True
             elif result == self.human_player:
                 print('human win')
-                break
+                self.gameover = True
             elif result == -1:
                 print('draw')
-                break
-            if self.current_player == self.ai_player:
+                self.gameover = True
+            if self.current_player == self.ai_player and not self.gameover:
                 # do AI thinking
                 # self.table[0, 0] = self.ai_player
                 # while True:
@@ -167,16 +190,24 @@ class TicTacToe:
                     game_exit = True
                 if event.type == pg.MOUSEBUTTONUP:
                     pos = pg.mouse.get_pos()
-                    pos = pos[0] - self.offset[0], pos[1] - self.offset[1]
-                    cell = self.mouse_select(pos)
-                    if cell is not None:
+                    pos_offset = pos[0] - self.offset[0], pos[1] - self.offset[1]
+                    pos_b_offset = pos[0] - self.b_offset[0], pos[1] - self.b_offset[1]
+                    cell = self.mouse_select(pos_offset)
+                    if cell is not None and not self.gameover:
                         # print(cell)
                         self.process_click(cell)
 
                         # if self.check_final() != 0:
                         #     game_exit = True
+                    # check for replay button press
+                    if self.gameover:
+                        for button in self.game_buttons:
+                            if button.name == 'replay_button' and button.check_pressed(pos_b_offset):
+                                self.gameover = False
+                                self.table = np.zeros(shape=(self.table_size, self.table_size), dtype='int')
             self.screen.fill((60, 70, 90))
             self.screen.blit(self.background, self.offset)
+            self.screen.blit(self.button_surface, self.b_offset)
             pg.display.flip()
             self.clock.tick(30)
         pg.quit()
